4f514a673a10ab2533d0586f934625528bad40a7
rhdmAdmin <>
2020-02-12 15:39:33 -0200

    {/src/main/java/br/com/sulamerica/gto/model/Usuario.java}

--- src/main/java/br/com/sulamerica/gto/model/Usuario.java
+++ src/main/java/br/com/sulamerica/gto/model/Usuario.java
@@ -6,12 +6,35 @@ package br.com.sulamerica.gto.model;
 
 public class Usuario implements java.io.Serializable {
 
-    static final long serialVersionUID = 1L;
+	static final long serialVersionUID = 1L;
 
-    public Usuario() {
-    }
+	@org.kie.api.definition.type.Label(value = "Nome do usuario")
+	private java.lang.String nome;
+	@org.kie.api.definition.type.Label(value = "Estado do usuario")
+	private java.lang.String estado;
 
+	public Usuario() {
+	}
 
+	public java.lang.String getNome() {
+		return this.nome;
+	}
 
+	public void setNome(java.lang.String nome) {
+		this.nome = nome;
+	}
+
+	public java.lang.String getEstado() {
+		return this.estado;
+	}
+
+	public void setEstado(java.lang.String estado) {
+		this.estado = estado;
+	}
+
+	public Usuario(java.lang.String nome, java.lang.String estado) {
+		this.nome = nome;
+		this.estado = estado;
+	}
 
 }
